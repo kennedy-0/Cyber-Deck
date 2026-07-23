b6a5cf6df3f673d8894216d5148fbb7f6fd3e2ca
Cyber Wanderer <danielkennedi121@gmail.com>
2026-07-23 01:08:46 -0300

    Implement cache clearing script
    Add a script to clear cache every 30 seconds

--- codigos/cache/limpar_cache.py
+++ codigos/cache/limpar_cache.py
@@ -1,1 +1,10 @@
+import os
+import time
+
+while True:
+        os.system("sync")
+        with open("/proc/sys/vm/drop_caches", "w") as f:
+                f.write("3\n")
+
+        time.sleep(30)
 
